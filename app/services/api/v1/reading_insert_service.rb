21f3b9ae7989cc860f2b951192c236b52c60bf80
Jean Brito <jeanfbrito@gmail.com>
2019-12-27 02:44:11 -0300

    fix timestamp usage on reading insert

--- app/services/api/v1/reading_insert_service.rb
+++ app/services/api/v1/reading_insert_service.rb
@@ -4,13 +4,13 @@ module Api
   module V1
     class ReadingInsertService
       def initialize(params)
-        @data = params[:reading].to_h
-        @device ||= ::Device.find_by!(write_key: params[:write_key])
+        @data = params[:reading][:data].to_h
+        @device = ::Device.find_by!(write_key: params[:write_key])
       end
 
       def insert
         datetime = @data[:timestamp].present? ? Time.at(@data[:timestamp]) : Time.now
-        ::Reading.create(device: @device, datetime: datetime, data: @data[:data] )
+        ::Reading.create(device: @device, datetime: datetime, data: @data )
       end
     end
   end
